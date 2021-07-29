a5606913c15c6be4baab7316bda390cd55e76e73
Tanat <ttangun1@gmail.com>
2021-07-29 16:26:26 +0700

    try: better way to change text color

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ const color = document.querySelector('#color')
 let prev = ""
 let state = false;
 btn_toggle.onclick = () => {
-  // your code here
-  if (state == false) {
-    prev = author.innerText
-    const w = author.innerText.split(' ')
-    let v = parseInt(w[0]) + parseInt(length.value)
-    author.innerText = v
-    state = true
-  }
-  else {
-    author.innerText = prev
-    state = false
-  }
+    // your code here
+    if (state == false) {
+        prev = author.innerText
+        const w = author.innerText.split(' ')
+        let v = parseInt(w[0]) + parseInt(length.value)
+        author.innerText = v
+        state = true
+    }
+    else {
+        author.innerText = prev
+        state = false
+    }
 }
 
 // more codes for Search and Reset buttons here
@@ -32,38 +32,28 @@ let org = text.innerText
 let s = false
 
 search.onclick = () => {
-  let arr  = text.innerText.split(' ')
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length >= length.value) {
-      arr[i] = "<span style=\"color:" + color.value + ";\">" + arr[i] + "</span>"
-    }
-  } 
-
-  const t = arr.join(' ')
-
-  text.innerHTML = t
-
-  s = true
-}
-
-color.oninput = () => {
-  if (s == true) {
-    let arr  = text.innerText.split(' ')
+    let arr = text.innerText.split(' ')
 
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i].length >= length.value) {
-        arr[i] = "<span style=\"color:" + color.value + ";\">" + arr[i] + "</span>"
-      }
-    } 
+        if (arr[i].length >= length.value) {
+            arr[i] = "<span class=\"clr\">" + arr[i] + "</span>"
+        }
+    }
 
+    document.documentElement.style.setProperty('--clr', color.value)
     const t = arr.join(' ')
 
     text.innerHTML = t
-  }
 
+    s = true
+}
+
+color.oninput = () => {
+    if (s == true) {   
+        document.documentElement.style.setProperty('--clr', color.value)
+    }
 }
 
 reset.onclick = () => {
-  text.innerHTML = org
+    text.innerHTML = org
 }
