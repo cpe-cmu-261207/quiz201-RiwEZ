2b6f52a9c5ac0059ce1ac1139454142d0e3810b6
Tanat <ttangun1@gmail.com>
2021-07-29 14:16:07 +0700

    try: 1

--- script.js
+++ script.js
@@ -5,9 +5,49 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
-
+let prev = ""
+let state = false;
 btn_toggle.onclick = () => {
   // your code here
+  if (state == false) {
+    prev = author.innerText
+    const w = author.innerText.split(' ')
+    let v = parseInt(w[0]) + parseInt(length.value)
+    author.innerText = v
+    state = true
+  }
+  else {
+    author.innerText = prev
+    state = false
+  }
 }
 
 // more codes for Search and Reset buttons here
+
+const search = document.getElementById('search')
+const reset = document.getElementById('reset')
+const text = document.getElementById('text')
+
+let org = text.innerText
+
+search.onclick = () => {
+  let arr  = text.innerText.split(' ')
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i].length >= length.value) {
+      arr[i] = "<span style=\"color:" + color.value + ";\">" + arr[i] + "</span>"
+    }
+  }
+
+  const t = arr.join(' ')
+
+  text.innerHTML = t
+}
+
+color.onclick = () => {
+}
+
+reset.onclick = () => {
+  text.innerHTML = org
+}
